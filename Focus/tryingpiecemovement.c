53dd33289b4bf8a6733ed24c626986e35af18d51
Azubir <57294100+Azubir@users.noreply.github.com>
2020-04-18 20:54:01 +0100

    Add files via upload

---
+++ Focus/tryingpiecemovement.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+int r,r2,c,c2,getcount,flag==0;
+/*
+
+if(c==c2){
+if(getcount==1){
+int one[]={r+1,r-1};
+}else if(getcount==2){
+int two[]={r+1,r-1,r+2,r-2};
+}else if(getcount==3){
+int three[]={r+1,r-1,r+2,r-2,r+3,r-3};
+}else if(getcount==4){
+int four[]={r+1,r-1,r+2,r-2,r+3,r-3,r+4,r-4};
+}else if(getcount==1){
+int five[]={r+1,r-1,r+2,r-2,r+3,r-3,r+4,r-4,r+5,r-5};
+}
+}
+*/
+
+/*
+else if(r==r2){
+if(getcount==1){
+int one[]={c+1,c-1};
+}else if(getcount==2){
+int two[]={c+1,c-1,c+2,c-2};
+}else if(getcount==3){
+int three[]={c+1,c-1,c+2,c-2,c+3,c-3};
+}else if(getcount==4){
+int four[]={c+1,c-1,c+2,c-2,c+3,c-3,c+4,c-4};
+}else if(getcount==1){
+int five[]={c+1,c-1,c+2,c-2,c+3,c-3,c+4,c-4,c+5,c-5};
+}
+}
+*/
+
+
+
+/*
+else if(getcount==2){
+if(r2-r==1){
+int two[]={c+1,c-1};
+}
+}
+*/
+
+/*
+else if(getcount==3){
+if(r2-r==1){
+int three[]={c+1,c-1,c+2,c-2};
+}else if(r2-r1==2){
+int three[]={c+1,c-1};
+}
+}
+*/
+
+/*
+else if(getcount==4){
+if(r2-r==1){
+int four[]={c+1,c-1,c+2,c-2,c+3,c-3};
+}else if(r2-r==2){
+int four[]={c+1,c-1,c+2,c-2};
+}else if(r2-r==3){
+int four[]={c+1,c-1};
+}
+}*/
+
+/*
+else if(getcount==5){
+if(r2-r==1){
+int five[]={c+1,c-1,c+2,c-2,c+3,c-3,c+4,c-4};
+}else if(r2-r==2){
+int five[]={c+1,c-1,c+2,c-2,c+3,c-3};
+}else if(r2-r==3){
+int five[]={c+1,c-1,c+2,c-2};
+}else if(r2-r==4){
+int five[]={c+1,c-1,c+2,c-2};
+}
+}
+*/
+
+
+return 0;
+}
