a4d2eb45a90569a29f2438b57bc9ee040b7b4c72
Azubir <57294100+Azubir@users.noreply.github.com>
2020-05-02 14:42:01 +0100

    Update CheckWinner.c

--- Focus/Focus/Focus/Focus/CheckWinner.c
+++ Focus/Focus/Focus/Focus/CheckWinner.c
@@ -10,15 +10,15 @@
 int checkwinner(square board[BOARD_SIZE][BOARD_SIZE], struct square * HEAD[BOARD_SIZE][BOARD_SIZE],player players[PLAYERS_NUM]){//piececolor is enemy colour
     int green=0,red=0,l=0;//0 flag no more top green and red wins / 1 red no more top red and green wins/ 0 flag continue
     for(int i=0;i<BOARD_SIZE;i++){
-        for(int j=0;j<BOARD_SIZE;j++){
-            if(board[i][j].type == VALID) {
-                if(/*board[i][j].stack == NULL ||*/HEAD[i][j]==NULL)
+        for(int j=0;j<BOARD_SIZE;j++){//in order to check each square of the board
+            if(board[i][j].type == VALID) {//the square has to be valid in the first place
+                if(/*board[i][j].stack == NULL ||*/HEAD[i][j]==NULL)//if theres no pieces we dont do anything
                     l++;
                 else{
-                    if (board[i][j].stack->p_color == GREEN) {
+                    if (board[i][j].stack->p_color == GREEN) {//we keep count of the number of top pieces that are green 
                        green++;
                     }
-                    else {
+                    else {//we keep count of the number of top pieces that are red
                         red++;
                     }
                 }
@@ -34,12 +34,11 @@ int checkwinner(square board[BOARD_SIZE][BOARD_SIZE], struct square * HEAD[BOARD
 
 
 
-    if(green==0){
-        printf("%s WINS\nCOLOUR: RED\nCAPTURED PIECES(ENEMYS): %d\nRESERVED PIECES: %d",players[0].name,players[0].captured_pieces,players[0].own_pieces_stock);
+    if(green==0 && players[0].own_pieces_stock==0){//check if theres no more green top colors and the players doesnt have any received pieces
+        printf("%s WINS\nCOLOUR: RED\nCAPTURED PIECES(ENEMYS): %d\nRESERVED PIECES: %d",players[0].name,players[0].captured_pieces,players[0].own_pieces_stock);//prints out the winner information
         return 0;
-    }else if(red==0){
-        printf("GREEN WINS");
-        printf("%s WINS\nCOLOUR: GREEN\nCAPTURED PIECES(ENEMYS): %d\nRESERVED PIECES: %d",players[1].name,players[1].captured_pieces,players[1].own_pieces_stock);
+    }else if(red==0 && players[1].own_pieces_stock==0){//check if theres no more red top colors and the players doesnt have any received pieces
+        printf("%s WINS\nCOLOUR: GREEN\nCAPTURED PIECES(ENEMYS): %d\nRESERVED PIECES: %d",players[1].name,players[1].captured_pieces,players[1].own_pieces_stock);//prints out the winner information
         return 0;
     }else{
         return 1;
